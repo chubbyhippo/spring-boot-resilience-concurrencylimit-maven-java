c75ed5cbbdb18a209b3fc465a3b896856530bf3a
chubbyhippo <mkmsacc@outlook.com>
2026-05-08 09:06:32 +0700

    Add concurrency limit and logging to `/hello` endpoint in `HelloController`.

--- src/main/java/io/github/chubbyhippo/demo/HelloController.java
+++ src/main/java/io/github/chubbyhippo/demo/HelloController.java
@@ -1,12 +1,19 @@
 package io.github.chubbyhippo.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class HelloController {
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 	@GetMapping("/hello")
-	public String hello() {
+    @ConcurrencyLimit(1)
+	public String hello() throws InterruptedException {
+        Thread.sleep(1000);
+
 		return "Hello World!";
 	}
 }
