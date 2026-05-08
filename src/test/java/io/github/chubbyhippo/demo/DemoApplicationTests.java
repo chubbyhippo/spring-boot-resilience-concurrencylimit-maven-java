3e394f2aba2fe0a5d83c05e777ce85af51bdbbe6
chubbyhippo <mkmsacc@outlook.com>
2026-05-08 08:53:09 +0700

    Add integration test for `/hello` endpoint with MockMvc configuration

--- src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
+++ src/test/java/io/github/chubbyhippo/demo/DemoApplicationTests.java
@@ -1,13 +1,24 @@
 package io.github.chubbyhippo.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvcTester mockMvcTester;
+
+    @Test
+    void testGetHello() {
+        mockMvcTester.get()
+                .uri("/hello")
+                .assertThat()
+                .hasStatusOk();
+    }
 
 }
